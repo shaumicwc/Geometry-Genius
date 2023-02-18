e1318b9d5b3e5bcca1b1fa8bb72063ce31d38297
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-02-18 07:53:34 +0600

    js file added

--- dom.js
+++ dom.js
@@ -1,27 +1,3 @@
-// function 1:
-function getInputValue(id) {
-    const valueStr = document.getElementById(id).value;
-    const value = parseFloat(valueStr);
-return value;
-}
-
-// function 2:
-function displayResult(id, title, result) {
-    const parentElement = document.getElementById(id);
-    const createdElement = document.createElement('li');
-    createdElement.classList.add('mb-6');
-    createdElement.innerHTML = `
-    <div class="flex justify-between">
-        <p>${title}</p>
-        <p>${result}cm<sup>2</sup></p>
-        <button class="bg-blue-600 px-2 py-1 text-white rounded-md hover:bg-blue-400">Covert to m<sup>2</sup>
-        </button>
-    </div>
-    `
-    parentElement.appendChild(createdElement);
-}
-
-
 document.getElementById('triangle-calculate-btn').addEventListener('click', function () {
     const cardTitle = document.getElementById('triangle-title').innerText;
     const base = getInputValue('triangle-b');
